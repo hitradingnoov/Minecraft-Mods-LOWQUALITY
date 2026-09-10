5210d7b1f6402206ae0e8e282f11d26e4dcec78c
hitradingnoov <116665105+hitradingnoov@users.noreply.github.com>
2026-09-10 15:01:05 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,22 @@
+// config.js
+const BASE_URL = 'https://buxify.site';
+
+module.exports = {
+    PORT: process.env.PORT || 3000,
+    google: {
+        clientId: '235420484535-vd39irnfuk20jhg8n9jv3gqoaej2o3le.apps.googleusercontent.com',
+        clientSecret: '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',
+        callbackUrl: `${BASE_URL}/auth/google/callback`
+    },
+    roblox: {
+        clientId: '4438582113529593979',
+        clientSecret: 'RBX-V6hUjS0xEUKsnVvb3hMqO_f8NG1Da4Zuz68mA_1DWXbuw9WhSvxuEn5nxC0rcr2K',
+        callbackUrl: `${BASE_URL}/auth/roblox/callback`
+    },
+    discord: {
+        clientId: '1544421065860583434',
+        clientSecret: 'qqFkE7CJMVWEhAfsejbaVqj04vY2xYUO',
+        publicKey: '30efc312e5eb200d2ee6205d93cac8f442ef072b7297d86569fd9288e9bb88c6',
+        callbackUrl: `${BASE_URL}/auth/discord/callback`
+    }
+};
